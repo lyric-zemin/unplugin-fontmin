17fb495472c5e931d160d2b1eb04a0dc7370f47e
lyric <2443835100@qq.com>
2024-11-28 11:40:49 +0800

    test: update

--- test/fontmin.test.ts
+++ test/fontmin.test.ts
@@ -1,8 +1,12 @@
+import { rmdir } from 'node:fs/promises'
+import { resolve } from 'node:path'
+import { cwd } from 'node:process'
 import { describe, expect, it } from 'vitest'
 import Fontmin from 'fontmin'
 
 describe('fontmin', () => {
   it('path nested', async () => {
+    await rmdir(resolve(cwd(), 'test/output'), { recursive: true })
     const fontmin = new Fontmin()
     fontmin.src('test/**/*.{ttf,otf}').dest('test/output')
     fontmin.use(Fontmin.glyph({ text: 'hello' }))
